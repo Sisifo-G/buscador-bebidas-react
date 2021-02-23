adc67617feb4fd500e5b08f081a348f56ad6dbf3
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-02-23 07:42:55 -0500

    Creando el State para los campos del formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,8 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CategoriasContext } from "../context/CategoriasContext";
 
 const Formulario = () => {
+  const [busqueda, setBusqueda] = useState({
+    nombre: "",
+    categoria: "",
+  });
+
   const { categorias } = useContext(CategoriasContext);
+
+  // Función para leer los contenidos del formulario
+  const obtenerDatosReceta = (e) => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <form action="" className="col-12">
@@ -17,10 +30,15 @@ const Formulario = () => {
             className="form-control mt-3"
             type="text"
             placeholder="Busca por Ingrediente"
+            onChange={obtenerDatosReceta}
           />
         </div>
         <div className="col-md-4">
-          <select className="form-control mt-3" name="categoria">
+          <select
+            className="form-control mt-3"
+            name="categoria"
+            onChange={obtenerDatosReceta}
+          >
             <option value=""> -- Selecciona Categoría --</option>
             {categorias.map((categoria) => (
               <option key={categoria.strCategory} value={categoria.strCategory}>
